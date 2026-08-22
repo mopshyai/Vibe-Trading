8225ac48674c918932be70c07b2f510410b146fb
Manvendra Kumar <mopshyai@gmail.com>
2026-08-22 11:42:53 -0600

    feat(options): export decision risk and paper APIs
    Signed-off-by: Manvendra Kumar <mopshyai@gmail.com>

--- agent/src/options_market/__init__.py
+++ agent/src/options_market/__init__.py
@@ -1,5 +1,11 @@
 """Institutional-style U.S. options market research pipeline."""
 
+from .catalyst import (
+    CatalystEvent,
+    CatalystScoreConfig,
+    directional_catalyst_scores,
+    score_catalysts,
+)
 from .chart_screen import (
     ChartScreenConfig,
     ChartSnapshot,
@@ -12,27 +18,55 @@ from .databento_adapter import (
     DatabentoHistoricalConfig,
     OSIContract,
 )
+from .ev import (
+    ExpectedValueConfig,
+    candidate_ev_gate,
+    choose_score_threshold,
+    empirical_expected_value,
+)
 from .outcomes import OutcomeConfig, calibrate_score_buckets, label_long_option_path
+from .paper_execution import (
+    PaperExecutionConfig,
+    build_paper_option_order,
+    submit_paper_option_order,
+)
 from .pipeline import OptionsMarketPipelineConfig, combine_rankings
+from .risk import PortfolioRiskConfig, assess_portfolio_risk
 from .store import OptionsResearchStore
 from .universe import USListing, fetch_us_listed_universe, parse_symbol_directory
+from .walkforward import WalkForwardConfig, evaluate_walk_forward
 
 __all__ = [
+    "CatalystEvent",
+    "CatalystScoreConfig",
     "ChartScreenConfig",
     "ChartSnapshot",
     "DatabentoHistoricalAdapter",
     "DatabentoHistoricalConfig",
+    "ExpectedValueConfig",
     "OSIContract",
     "OptionsMarketPipelineConfig",
     "OptionsResearchStore",
     "OutcomeConfig",
+    "PaperExecutionConfig",
+    "PortfolioRiskConfig",
     "USListing",
+    "WalkForwardConfig",
+    "assess_portfolio_risk",
+    "build_paper_option_order",
     "calibrate_score_buckets",
+    "candidate_ev_gate",
+    "choose_score_threshold",
     "combine_rankings",
     "compute_chart_snapshot",
+    "directional_catalyst_scores",
+    "empirical_expected_value",
+    "evaluate_walk_forward",
     "fetch_us_listed_universe",
     "label_long_option_path",
     "parse_symbol_directory",
     "rank_chart_snapshots",
     "scan_market_frames",
+    "score_catalysts",
+    "submit_paper_option_order",
 ]
