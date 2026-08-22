68f4c187b262b0172ce4d017fb95d6af4324d00a
Manvendra Kumar <mopshyai@gmail.com>
2026-08-22 11:14:55 -0600

    feat(options): expose market engine package
    Signed-off-by: Manvendra Kumar <mopshyai@gmail.com>

---
+++ agent/src/options_market/__init__.py
@@ -0,0 +1,24 @@
+"""Institutional-style U.S. options market research pipeline."""
+
+from .chart_screen import (
+    ChartScreenConfig,
+    ChartSnapshot,
+    compute_chart_snapshot,
+    rank_chart_snapshots,
+    scan_market_frames,
+)
+from .pipeline import OptionsMarketPipelineConfig, combine_rankings
+from .universe import USListing, fetch_us_listed_universe, parse_symbol_directory
+
+__all__ = [
+    "ChartScreenConfig",
+    "ChartSnapshot",
+    "OptionsMarketPipelineConfig",
+    "USListing",
+    "combine_rankings",
+    "compute_chart_snapshot",
+    "fetch_us_listed_universe",
+    "parse_symbol_directory",
+    "rank_chart_snapshots",
+    "scan_market_frames",
+]
