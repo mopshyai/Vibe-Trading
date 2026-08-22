5482d7a7ac7fb30902aa75a91626e30f4e0f1d80
Manvendra Kumar <mopshyai@gmail.com>
2026-08-22 16:05:17 -0600

    feat(paper): export supervised lifecycle runtime
    Signed-off-by: Manvendra Kumar <mopshyai@gmail.com>

--- agent/src/trading_platform/__init__.py
+++ agent/src/trading_platform/__init__.py
@@ -22,6 +22,15 @@ from .models import (
     SystemIdentity,
     TradingDeskSnapshot,
 )
+from .paper_lifecycle import (
+    PaperLifecycleConfig,
+    append_reconciled_order_status,
+    fetch_alpaca_market_clock,
+    fetch_alpaca_order,
+    fetch_current_option_quote,
+    run_paper_option_lifecycle,
+    sync_paper_order_lifecycle,
+)
 from .portfolio_risk import (
     AccountRiskConfig,
     assess_account_portfolio_risk,
@@ -45,6 +54,7 @@ __all__ = [
     "JournalStage",
     "MarketCalendarDependencyError",
     "OpportunityCard",
+    "PaperLifecycleConfig",
     "PlatformEnvironment",
     "PlatformEvent",
     "RiskSummary",
@@ -52,10 +62,16 @@ __all__ = [
     "TradingDeskSnapshot",
     "TradingPlatformService",
     "TradingPlatformStore",
+    "append_reconciled_order_status",
     "assess_account_portfolio_risk",
     "evaluate_data_freshness",
+    "fetch_alpaca_market_clock",
+    "fetch_alpaca_order",
+    "fetch_current_option_quote",
     "risk_summary_from_report",
+    "run_paper_option_lifecycle",
     "stage_for_decision",
+    "sync_paper_order_lifecycle",
     "xnys_session_calendar",
     "xnys_trading_session",
 ]
